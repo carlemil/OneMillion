882efc12683ab20f8fd2b4123af940bc5fb628cc
Carl-Emil Kjellstrand <carl-emil.kjellstrand@sonymobile.com>
2014-08-11 13:49:18 +0200

    better currency selection

--- src/se/kjellstrand/onemillion/MainActivity.java
+++ src/se/kjellstrand/onemillion/MainActivity.java
@@ -1,10 +1,11 @@
+
 package se.kjellstrand.onemillion;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Type;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,127 +24,120 @@ import android.widget.Spinner;
 import android.widget.TextView;
 
 import com.google.gson.Gson;
-import com.google.gson.reflect.TypeToken;
 
 public class MainActivity extends Activity implements OnItemSelectedListener {
 
-	private static final float ONE_MILLION = 1000000f;
+    private static final float ONE_MILLION = 1000000f;
 
-	private Gson gson = new Gson();
+    private Gson gson = new Gson();
 
-	private ExchangeRates exchangeRates;
-	
-	private final List<String> spinnerList = new ArrayList<String>();
+    private ExchangeRates exchangeRates;
 
-	private Map<String, String> currencies = new HashMap<String, String>();
+    private final List<String> currencyList = new ArrayList<String>();
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
+    private Map<String, String> currencies = new HashMap<String, String>();
 
-		setContentView(R.layout.main);
+    private Map<String, String> reverseMapCurrencies = new HashMap<String, String>();
 
-		String json = readFile(R.raw.exchange_rates);
-		exchangeRates = gson.fromJson(json, ExchangeRates.class);
+    private String baseCurrency = "United States Dollar";
 
-		json = readFile(R.raw.currencies);
-		currencies = gson.fromJson(json, currencies.getClass());
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
 
-		// Spinner element
-		final Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        setContentView(R.layout.main);
 
-		// Spinner click listener
-		spinner.setOnItemSelectedListener(this);
+        String json = readFile(R.raw.exchange_rates);
+        exchangeRates = gson.fromJson(json, ExchangeRates.class);
 
-		// Spinner Drop down elements
-		for (Entry<String, String> entry : currencies.entrySet()) {
-			spinnerList.add(entry.getKey());
-		}
+        json = readFile(R.raw.currencies);
+        currencies = gson.fromJson(json, currencies.getClass());
 
-		// Creating adapter for spinner
-		ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
-				android.R.layout.simple_spinner_item, spinnerList);
+        // Spinner element
+        final Spinner spinner = (Spinner) findViewById(R.id.spinner);
 
-		// Drop down layout style - list view with radio button
-		dataAdapter
-				.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // baseCurrency = spinnerList.get(spinner.getSelectedItemPosition());
 
-		// attaching data adapter to spinner
-		spinner.setAdapter(dataAdapter);
+        // Spinner click listener
+        spinner.setOnItemSelectedListener(this);
 
-		findViewById(R.id.ok_button).setOnClickListener(new OnClickListener() {
-			@Override
-			public void onClick(View v) {
-				String amountString = ((EditText) findViewById(R.id.amount))
-						.getText().toString();
+        // Spinner Drop down elements
+        for (Entry<String, String> entry : currencies.entrySet()) {
+            reverseMapCurrencies.put(entry.getValue(), entry.getKey());
+            currencyList.add(entry.getValue());
+        }
+        Collections.sort(currencyList);
 
-				if (amountString != null && amountString.length() > 0) {
-					float amount = Float.parseFloat(amountString);
+        // Creating adapter for spinner
+        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, currencyList);
 
-					String base = spinnerList.get(spinner
-							.getSelectedItemPosition());
-					String cur = currencies.get(getBestMatchingCurrency(amount,
-							base));
-					String resultFormat = getResources().getString(
-							R.string.result_text);
-					String text = String
-							.format(resultFormat, amount, currencies.get(base), cur);
+        // Drop down layout style - list view with radio button
+        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
-					((TextView) findViewById(R.id.result)).setText(text);
-				}
-			}
+        // attaching data adapter to spinner
+        spinner.setAdapter(dataAdapter);
 
-		});
-	}
+        findViewById(R.id.ok_button).setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String amountString = ((EditText) findViewById(R.id.amount)).getText().toString();
 
-	public void onItemSelected(AdapterView<?> parent, View view, int pos,
-			long id) {
-		final Spinner spinner = (Spinner) findViewById(R.id.spinner);
-		String base = spinnerList.get(spinner
-				.getSelectedItemPosition());
-		((TextView) findViewById(R.id.base)).setText(currencies.get(base));
-	}
+                if (amountString != null && amountString.length() > 0) {
+                    float amount = Float.parseFloat(amountString);
 
-	public void onNothingSelected(AdapterView<?> parent) {
-		// Another interface callback
-	}
+                    String cur = currencies.get(getBestMatchingCurrency(amount, baseCurrency));
+                    String resultFormat = getResources().getString(R.string.result_text);
+                    String text = String.format(resultFormat, amount, baseCurrency, cur);
 
-	private String getBestMatchingCurrency(float amount, String base) {
-		String bestMatchCurrency = "USD";
-		float baseRate = exchangeRates.rates.get(base);
-		for (Entry<String, Float> entry : exchangeRates.rates.entrySet()) {
-			if (getDistanceToOneMillion(
-					exchangeRates.rates.get(bestMatchCurrency), amount,
-					baseRate) > getDistanceToOneMillion(entry.getValue(),
-					amount, baseRate)) {
-				bestMatchCurrency = entry.getKey();
-				Log.d("TAG", "----------------------");
-			}
-		}
+                    ((TextView) findViewById(R.id.result)).setText(text);
+                }
+            }
+        });
 
-		return bestMatchCurrency;
-	}
+        // Select the default currency
+        spinner.setSelection(currencyList.lastIndexOf(baseCurrency));
+    }
 
-	private float getDistanceToOneMillion(Float rate, float amount,
-			float baseRate) {
-		float distance = Math.abs((rate * amount / baseRate) - ONE_MILLION);
-		Log.d("TAG", "dist: " + distance + " rate " + rate + " amount "
-				+ amount + " base " + baseRate);
-		return distance;
-	}
+    public void onItemSelected(AdapterView<?> parent, View view, int pos, long id) {
+        final Spinner spinner = (Spinner) findViewById(R.id.spinner);
+        baseCurrency = currencyList.get(spinner.getSelectedItemPosition());
+    }
 
-	private String readFile(int resId) {
-		BufferedReader r = new BufferedReader(new InputStreamReader(
-				getResources().openRawResource(resId)));
-		StringBuilder total = new StringBuilder();
-		String line;
-		try {
-			while ((line = r.readLine()) != null) {
-				total.append(line);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-		return total.toString();
-	}
+    public void onNothingSelected(AdapterView<?> parent) {
+        // Another interface callback
+    }
+
+    private String getBestMatchingCurrency(float amount, String base) {
+        String bestMatchCurrency = "USD";
+        float baseRate = exchangeRates.rates.get(reverseMapCurrencies.get(base));
+        for (Entry<String, Float> entry : exchangeRates.rates.entrySet()) {
+            if (getDistanceToOneMillion(exchangeRates.rates.get(bestMatchCurrency), amount, baseRate) > getDistanceToOneMillion(
+                    entry.getValue(), amount, baseRate)) {
+                bestMatchCurrency = entry.getKey();
+                Log.d("TAG", "----------------------");
+            }
+        }
+
+        return bestMatchCurrency;
+    }
+
+    private float getDistanceToOneMillion(Float rate, float amount, float baseRate) {
+        float distance = Math.abs((rate * amount / baseRate) - ONE_MILLION);
+        Log.d("TAG", "dist: " + distance + " rate " + rate + " amount " + amount + " base " + baseRate);
+        return distance;
+    }
+
+    private String readFile(int resId) {
+        BufferedReader r = new BufferedReader(new InputStreamReader(getResources().openRawResource(resId)));
+        StringBuilder total = new StringBuilder();
+        String line;
+        try {
+            while ((line = r.readLine()) != null) {
+                total.append(line);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return total.toString();
+    }
 }
